a4078c28966570f5bea100c827b414678cc3e203
Kiran Tomlinson <kirantomlinson@gmail.com>
2018-03-10 10:54:27 -0600

    Tweaks to drone kalman

--- soloPRestimate.m
+++ soloPRestimate.m
@@ -25,19 +25,16 @@ H = [1 0 0 0; 0 1 0 0];
 % Control input matrix: sensor input matrix 
 B = [0 0; 0 0; 1 0; 0 1];
 
-
-
 % Error covariance matrix
 P = eye(4); 
 
 
-
 % Process noise covariance matrix. Represents system noisiness
-varQ = 0.02;
+varQ = 0.001;
 Q = [varQ 0  0 0; 0 varQ 0 0; 0 0 varQ 0; 0 0 0 varQ];
 
 % Measurement noise covariance matrix. Represents measurement noisiness
-varR = 0.02;
+varR = 0.1;
 R = [varR 0; 0 varR ];
 
 
@@ -52,25 +49,27 @@ m = length(lat);
 coord = ones(m,2);
 coord(1,1) = 0; coord(1,2) = 0 ;
 
+
 for i=1:m-1
     longdif = long(i)-long(i+1);
     
     latdif = lat(i)-lat(i+1);
     coord(i+1,1) =  coord(i,1)+ 111111*longdif;coord(i+1,2) =coord(i,2)+111111*latdif;
 end
-    
+
+noisyCoord = coord + normrnd(0, varR, m, 2);
 
 
 for i=1:m-1
+%     u = [sin(pitch(i)+0.05)*cos(roll(i)-0.05); -sin(roll(i)-0.05)];
     u = [sin(pitch(i))*cos(roll(i)); -sin(roll(i))];
-    
+
     t = t + dt;
-    u = -20;
     
     xModelPR = F*x + B*u + normrnd(0, diag(Q));
     
     times = [times t];
-    states = [states x];
+    states = [states coord(i,1:2)'];
     
     xModelPR = F*xModelPR + B*u;
     modelPR= [modelPR xModelPR];
@@ -83,7 +82,7 @@ for i=1:m-1
     K = P*H.'*inv(H*P*H.' + R);
     
     % Measurement
-    z = coord(i,1:2)';
+    z = noisyCoord(i,1:2)';
 %     z = H * xcur + normrnd(0, diag(R));
     measurements = [measurements z];
     
@@ -103,10 +102,10 @@ end
 hold on;
 
 % Position
-% plot(times, measurements(1,:), 'cyan');
+plot( measurements(1,:), measurements(2,:), 'cyan');
 % plot(times, model(1,:), 'b');
-plot(times, states(1,:), 'black');
-plot(times, outputs(1,:), 'r');
+plot(states(1,:), states(2,:), 'black');
+plot(outputs(1,:), outputs(2,:), 'r');
 
 
 % Velocity
